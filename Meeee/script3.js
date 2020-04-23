f7da7ca5baf9f98fc97f4b96f5064e4cea167e21
SimranChadda <54142296+SimranChadda@users.noreply.github.com>
2020-04-23 14:47:31 +0530

    Add files via upload

---
+++ Meeee/script3.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".hamburger");
+const n = document.querySelector(".n");
+const links = document.querySelector(".links");
+
+
+hamburger.addEventListener("click",() =>{
+
+
+  n.classList.toggle("open");
+});
